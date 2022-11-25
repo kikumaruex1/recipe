3330016536623a659cf954b1c88c53f9e26a264e
kiku <Kikuchi.sho@technopro.com>
2022-11-25 12:00:57 +0900

    entityの作成

--- alrecipe/src/main/java/com/example/entity/Process.java
+++ alrecipe/src/main/java/com/example/entity/Process.java
@@ -1,5 +1,82 @@
 package com.example.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+/**
+ * 手順情報
+ */
+@Entity
+@Table(name = "PROCESSES")
 public class Process {
+
+	@Id
+    @SequenceGenerator(name = "PROCESSES_ID_GENERATOR", sequenceName = "PROCESSES_ID_SEQ", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "PROCESSES_ID_GENERATOR")
+	@Column(name = "ID")
+    private Long id;
+
+	@Column(name = "IMAGE")
+    private String image;
+
+	@Column(name = "PROCESS")
+    private String process;
+
+	@ManyToOne
+    @JoinColumn(name = "RECIPE_ID")
+    private Recipe recipe;
+
+	@Column(name = "RECIPESORT_ID")
+    private Long recipesort_id;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	public String getProcess() {
+		return process;
+	}
+
+	public void setProcess(String process) {
+		this.process = process;
+	}
+
+	public Recipe getRecipe() {
+		return recipe;
+	}
+
+	public void setRecipe(Recipe recipe) {
+		this.recipe = recipe;
+	}
+
+	public Long getRecipesort_id() {
+		return recipesort_id;
+	}
+
+	public void setRecipesort_id(Long recipesort_id) {
+		this.recipesort_id = recipesort_id;
+	}
+
+
+
 
 }
--- alrecipe/src/main/java/com/example/entity/Recipe.java
+++ alrecipe/src/main/java/com/example/entity/Recipe.java
@@ -1,5 +1,140 @@
 package com.example.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+/**
+ * レシピ情報
+ */
+@Entity
+@Table(name = "RECIPES")
 public class Recipe {
+
+	@Id
+    @SequenceGenerator(name = "RECIPES_ID_GENERATOR", sequenceName = "RECIPES_ID_SEQ", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "RECIPES_ID_GENERATOR")
+    @Column(name = "ID")
+	private Long id;
+
+	@Column(name = "DESCRIPTION")
+	private String description;
+
+	@Column(name = "NAME")
+	private String name;
+
+	@Column(name = "IMAGE")
+    private String image;
+
+	@Column(name = "MOVIE")
+    private String movie;
+
+	@Column(name = "TIME")
+    private double time;
+
+	@Column(name = "PRICE")
+    private int price;
+
+	@ManyToOne
+    @JoinColumn(name = "SUBCATEGORY_ID")
+    private Subcategory	 subcategory;
+
+	@ManyToOne
+    @JoinColumn(name = "CATEGORY_ID")
+    private Category category;
+
+	@ManyToOne
+    @JoinColumn(name = "ALCOHOL_ID")
+    private Alcohol alcohol;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getImage() {
+		return "/recipe-images/" + id + "/" + image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	public String getMovie() {
+		return movie;
+	}
+
+	public void setMovie(String movie) {
+		this.movie = movie;
+	}
+
+	public double getTime() {
+		return time;
+	}
+
+	public void setTime(double time) {
+		this.time = time;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public Subcategory getSubcategory() {
+		return subcategory;
+	}
+
+	public void setSubcategory(Subcategory subcategory) {
+		this.subcategory = subcategory;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+	public Alcohol getAlcohol() {
+		return alcohol;
+	}
+
+	public void setAlcohol(Alcohol alcohol) {
+		this.alcohol = alcohol;
+	}
+
+
+
+
 
 }
